5be7dfb9fa9edf37caf6a80cb6bf35f4d3aa7d2a
Vihanity <82359534+vihaan035@users.noreply.github.com>
2023-03-31 12:07:55 +0530

    initial commit after destroyed git file

---
+++ lottery/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  '0x2A28a07dBde15F484b0ecedF24C4597672205153',
+  // remember to change this to your own phrase!
+  'https://goerli.infura.io/v3/2b592848179348fc8df856eb760c49d5'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
